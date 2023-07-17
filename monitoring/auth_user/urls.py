def218371327d069269f4d68bf78df7e413442af
Юрий Данилов <yuradan0903@gmail.com>
2023-07-17 15:36:34 +0300

    ~all project

--- monitoring/auth_user/urls.py
+++ monitoring/auth_user/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.login_view, name='login'),
     path('profile/', views.profile, name='profile'),
     path('logout/', views.logout_view, name='logout'),
-    path('get_sensor_data/<str:sensor_name>', views.get_sensor_data, name='get_sensor_data'),
-    path('get_sensor_data_history/<str:sensor_name>', views.get_sensor_data_history, name='get_sensor_history_data'),
+    path('get_sensor_data/<str:sensor_name>/<str:username>', views.get_sensor_data, name='get_sensor_data'),
+    path('get_sensor_data_history/<str:sensor_name>/<str:username>', views.get_sensor_data_history, name='get_sensor_data_history'),
 ]
